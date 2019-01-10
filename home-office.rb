fa07469fa0fc67a3fdfac2828ee25d8bdb4b5ac5
smilanpersonal <46566139+smilanpersonal@users.noreply.github.com>
2019-01-10 14:49:18 +0100

    initial commit

---
+++ home-office.rb
@@ -0,0 +1,27 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+homeText = ["ma itthonrol, ", "ma homeoffice, ", "ma itthonrol vagyok, "]
+homeText = homeText.shuffle
+
+zText = ["Z megengedte", "Z engedelyevel", "Z tud rola"]
+zText = zText.shuffle
+
+uri = URI.parse("https://hooks.slack.com/services/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")
+request = Net::HTTP::Post.new(uri)
+request.content_type = "application/json"
+request.body = JSON.dump({
+  "text" => homeText[0] + zText[0]
+})
+
+req_options = {
+  use_ssl: uri.scheme == "https",
+}
+
+response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  http.request(request)
+end
+
+# response.code
+# response.body
