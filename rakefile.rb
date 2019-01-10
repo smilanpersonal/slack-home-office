b2d5619dec128595e3a767ba58a21e715a01f0ef
Milan Sarosac <milan.sarosac@jaystack.com>
2019-01-10 16:11:24 +0100

    bin folder

--- rakefile.rb
+++ rakefile.rb
@@ -2,14 +2,14 @@ task default: %w[homeoffice]
 
 task :homeoffice do
   if Time.now.monday?
-    ruby "home-office.rb"
+    ruby "bin/home-office.rb"
   elsif Time.now.tuesday?
-    ruby "home-office.rb"
+    ruby "bin/home-office.rb"
   elsif Time.now.wednesday?
-    ruby "home-office.rb"
+    ruby "bin/home-office.rb"
   elsif Time.now.thursday?
-    ruby "home-office.rb"
+    ruby "bin/home-office.rb"
   elsif Time.now.friday?
-    ruby "home-office.rb"
+    ruby "bin/home-office.rb"
   end
 end
